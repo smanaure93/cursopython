45572c980af4db2c02508b5d3afe9fc6556506fe
Sócrates Manaure <smanaure93@gmail.com>
2023-02-22 16:15:23 -0400

    booleanos

---
+++ 5.Booleanos/2.OperadoresComparacion.py
@@ -0,0 +1,15 @@
+firstNumber = 1000
+secondNumber = 500
+thirdNumber = 1000
+longestString = "Comparaciones de Strings"
+shortestString = "Comparacion de Strings"
+
+print(secondNumber > firstNumber)
+print(secondNumber < firstNumber)
+print(firstNumber >= thirdNumber)
+print(firstNumber <= thirdNumber)
+print(firstNumber == thirdNumber)
+print(firstNumber != thirdNumber)
+
+print(longestString > shortestString)
+print(shortestString > longestString)
